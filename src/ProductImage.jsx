4e967425ec931c3e88d9702daae306df31599eaa
Iosua Paul <babisca.paul1@gmail.com>
2024-03-17 21:07:47 +0200

    added carousel on hero section

--- src/ProductImage.jsx
+++ src/ProductImage.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
-const ProductImage = () => {
-    return <div className="product-image">Product Image</div>;
-};
+import Carousel from 'react-bootstrap/Carousel';
+import placeHolder from './assets/placeholder.jpg';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function ProductImage() {
+    return (
+        <Carousel data-bs-theme="dark" fade className='product-image'>
+            <Carousel.Item>
+                <img className="d-block w-100" src={placeHolder} alt="" />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className="d-block w-100" src={placeHolder} alt="" />
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className="d-block w-100" src={placeHolder} alt="" />
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    );
+}
 
 export default ProductImage;
