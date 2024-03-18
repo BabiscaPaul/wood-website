0df58e88def1de9c23161706fe6dfa44f2b24449
Iosua Paul <babisca.paul1@gmail.com>
2024-03-18 20:50:19 +0200

    footer done, main page done

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
 
       <MainPage></MainPage>
 
+      <Footer></Footer>
     </>
   );
 }
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,11 +1,37 @@
-// Footer.js
 import React from 'react';
-import './Styles/FooterStyles/Footer.css'
+import './Styles/FooterStyles/Footer.css';
 
 function Footer() {
   return (
     <footer className='footer'>
-      <p>This is the footer</p>
+      <div className="footer-main-content">
+        <div className="footer-about">
+          <h3>Your Company Name</h3>
+          <p>Dedicated to excellence and innovation in whatever we do.</p>
+        </div>
+        <div className="footer-links">
+          <h4>Quick Links</h4>
+          <ul>
+            <li><a href="/about">Poze Lemne</a></li>
+            <li><a href="/services">Pret Lemne</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li><a href="/contact">Informatii Utile</a></li>
+          </ul>
+        </div>
+        <div className="footer-contact">
+          <h4>Contact Us</h4>
+          <a href="tel: 0722 730 076">+40 722 730 076</a>
+          <br />
+          <a href="tel: 0722 730 076">+40 763 485 181</a>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <em>
+          <p>
+            © {new Date().getFullYear()} 'Numele Companiei. Toate drepturile rezervate.'
+          </p>
+        </em>
+      </div>
     </footer>
   );
 }
